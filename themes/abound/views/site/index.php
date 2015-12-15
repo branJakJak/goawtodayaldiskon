8d4177f1669a7f931ef15aec8adc787f667af7b3
kebyang <kelvo_link@yahoo.com>
2015-12-15 19:09:11 +0800

    added quick

--- themes/abound/views/site/index.php
+++ themes/abound/views/site/index.php
@@ -16,7 +16,24 @@ $baseUrl = Yii::app()->theme->baseUrl;
         <?php 
             $this->widget('zii.widgets.grid.CGridView', array(
               'dataProvider'=>$dataprovider,
-
+              'columns'=>array(
+                    array(
+                        'header'=>'Agent',
+                        'value'=>'$data["agent"]',
+                    ),
+                    array(
+                        'header'=>'Status',
+                        'value'=>'$data["status"]',
+                    ),
+                    array(
+                        'header'=>'Campaign ',
+                        'value'=>'$data["campaign_id"]',
+                    ),
+                    array(
+                        'header'=>'Action',
+                        'value'=>'<button onclick="disconnectUser(\'$data["agent"]\')" type="button" class="btn btn-default">disconnect</button>',
+                    ),
+                ),
             ));
         ?>
         <?php $this->endWidget(); ?>
