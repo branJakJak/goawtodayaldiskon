d1373407a4b438ed574212268e8ca6083f3db98b
kebyang <kelvo_link@yahoo.com>
2015-12-16 02:22:54 +0800

    added quick

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -20,6 +20,41 @@ class SiteController extends Controller
 			),
 		);
 	}
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+		);
+	}
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+			array('allow',
+				'actions'=>array('error','login','logout'),
+				'users'=>array('*'),
+			),
+			array('allow',
+				'actions'=>array('index','contact'),
+				'users'=>array('@'),
+			),
+			array('deny', 
+				'users'=>array('*'),
+			),
+		);
+	}
+
+
+
+
 
 	/**
 	 * This is the default 'index' action that is invoked
