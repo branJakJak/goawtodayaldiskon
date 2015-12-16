15a6eeebeb37c5eb1fcdaed4ef9536866baadb98
kebyang <kelvo_link@yahoo.com>
2015-12-16 21:41:33 +0800

    added quick

--- protected/config/console.php
+++ protected/config/console.php
@@ -3,26 +3,27 @@
 // This is the configuration for yiic console application.
 // Any writable CConsoleApplication properties can be configured here.
 return array(
-    'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-    'name'=>'My Console Application',
-    'import'=>array(
-        'ext.YiiMailer.YiiMailer',
-    ),
-    // application components
-    'components'=>array(
+	'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+	'name' => 'My Console Application',
+	'import' => array(
+		'ext.YiiMailer.YiiMailer',
+	),
+	// application components
+	'components' => array(
+
         'db'=>array(
-            'connectionString' => 'mysql:host=localhost;dbname=goautodialdiscon',
+            'connectionString' => 'mysql:host=localhost;dbname=roadto_agentmon',
             'emulatePrepare' => true,
-            'username' => 'root',
-            'password' => 'root',
-            'charset' => 'utf8',
+            'username' => 'roadto_agentmon',
+            'password' => 'hitman052529',
+            'charset' => 'utf8',  
         ),
-    ),
-    'modules'=>array(
+	),
+	'modules'=>array(
         'user'=>array(
                 'tableUsers' => 'users',
                 'tableProfiles' => 'profiles',
                 'tableProfileFields' => 'profiles_fields',
         ),
-    )    
+	)
 );
